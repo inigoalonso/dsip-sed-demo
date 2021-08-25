00ec5cb286e693d7644192ac999e8ef89736b6a9
Iñigo Alonso Fernandez <inigoalonso@inigoalonso.com>
2021-08-25 18:00:37 +0200

    display avatar and organization

--- src/components/profile/ViewProfile.vue
+++ src/components/profile/ViewProfile.vue
@@ -5,6 +5,15 @@
 
       <v-card class="px-3 elevation-0" text>
         <v-layout row wrap class="pa-3">
+          <v-flex md12>
+      <v-layout column align-center>
+        <v-flex class="mt-5 text-center">
+          <v-avatar size="200">
+            <img :src="avatarUrl" alt="Random avatar">
+          </v-avatar>
+        </v-flex>
+      </v-layout>
+          </v-flex>
           <v-flex xs12 md6>
             <div class="caption grey--text">Name</div>
             <div>{{ profile.name }}</div>
@@ -16,6 +25,10 @@
           <v-flex xs12 md6>
             <div class="caption grey--text">User ID</div>
             <div>{{ profile.user_id }}</div>
+          </v-flex>
+          <v-flex xs12 md6>
+            <div class="caption grey--text">Organization</div>
+            <div>{{ profile.organization }}</div>
           </v-flex>
         </v-layout>
         <v-divider></v-divider>
@@ -34,6 +47,11 @@ export default {
     return{
       profile: null
     }
+  },
+  computed: {
+    avatarUrl () {
+      return "https://avatars.dicebear.com/api/bottts/"+ this.profile.user_id +".svg"//+"?colors[]=green&colors[]=lightGreen&colors[]=lime"
+    },
   },
   created(){
     let ref = db.collection('users')
